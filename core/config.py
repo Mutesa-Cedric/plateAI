0379a31de375a8be899e09197f62e5d7fb8536be
MURANGWA Pacifique <pacifiquemurangwa001@gmail.com>
2024-09-28 02:34:48 +0200

    feat: meal checker api with object detection

--- core/config.py
+++ core/config.py
@@ -11,4 +11,6 @@ class Config:
     STT_IBM_API_KEY = os.environ.get('SPEECH_TO_TEXT_IBM_API_KEY')
     STT_IBM_URL = os.environ.get('SPEECH_TO_TEXT_IBM_URL')
 
+    GROQ_API_KEY = os.environ.get('GROQ_API_KEY')
+
     DEBUG = os.environ.get('DEBUG', 'True') == 'True'  # Convert to boolean
