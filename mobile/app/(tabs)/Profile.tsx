e823398b1a0b6c9531eff3c6632fa13d01b8b94f
Mutesa-Cedric <mutesacedric@gmail.com>
2024-09-28 12:24:19 +0200

    profile init

--- mobile/app/(tabs)/Profile.tsx
+++ mobile/app/(tabs)/Profile.tsx
@@ -1,7 +1,9 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import useAuth from '@/hooks/useAuth'
 
 export default function Profile() {
+    const { user } = useAuth();
     return (
         <View>
             <Text>Profile</Text>
