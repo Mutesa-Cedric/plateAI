7c967353b941583cb2bad5cd37f690d6cf8b37f8
israelmanzi <manziisrael99@gmail.com>
2024-09-29 07:29:52 +0200

    [fx]: get last 3 meals

--- core/services/advisor_service.py
+++ core/services/advisor_service.py
@@ -3,10 +3,11 @@ from config import Config
 from groq import Groq
 
 def get_advice(client, recent_meal, user, past_meals):
+    first_3_meals = past_meals[:3]
     prompt = (
-        f"We have a user ({user}), analyze this user's recent meal ({recent_meal})"
-        f"In not more than 1.5 lines, provide recommendations for improving the user's diet in relation to their purpose."
-        f"In response, make sure to use user's names to make it sound like you know him/her"
+        f"We have a user ({user}), analyze this user's recent meal ({recent_meal}) and past meals ({first_3_meals})."
+        f"In not more than 1.5 lines, provide recommendations for improving the user's diet in relation to their goal/purpose."
+        f"In response, make sure to user user's names to make it sound like you know him/her"
         f"Do not include any additional information or explanations."
     )
     
