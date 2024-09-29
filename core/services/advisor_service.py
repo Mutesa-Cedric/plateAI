94dcb6251ebff7e6ebbd5442d40baa98d3087ecc
israelmanzi <manziisrael99@gmail.com>
2024-09-29 05:51:55 +0200

    [fx] llama3-8b => llama-3.1-8b-instant

--- core/services/advisor_service.py
+++ core/services/advisor_service.py
@@ -12,7 +12,7 @@ def get_description(client, recent_meal, user, past_meals):
     
     chat_res = client.chat.completions.create(
         messages=[{"role": "user", "content": [{"type": "text", "text": prompt}]}],
-        model="llama3-8b-8192"
+        model="llama-3.1-8b-instant"
     )
     
     return chat_res.choices[0].message.content.strip()
