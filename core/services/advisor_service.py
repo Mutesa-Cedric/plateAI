eeccfd40f25186cd1e577f34dadcf5017c12c52c
israelmanzi <manziisrael99@gmail.com>
2024-09-29 06:06:27 +0200

    [fx]: add limiting parameters to llama model

--- core/services/advisor_service.py
+++ core/services/advisor_service.py
@@ -12,7 +12,12 @@ def get_description(client, recent_meal, user, past_meals):
     
     chat_res = client.chat.completions.create(
         messages=[{"role": "user", "content": [{"type": "text", "text": prompt}]}],
-        model="llama-3.1-8b-instant"
+        model="llama3-8b-8192",
+        temperature=0.7,
+        max_tokens=1024,
+        top_p=1,
+        stop=None,
+        stream=False,
     )
     
     return chat_res.choices[0].message.content.strip()
@@ -27,7 +32,12 @@ def get_advice(client, recent_meal, user, past_meals):
     
     chat_res = client.chat.completions.create(
         messages=[{"role": "user", "content": [{"type": "text", "text": prompt}]}],
-        model="llama-3.1-8b-instant"
+        model="llama3-8b-8192",
+        temperature=0.7,
+        max_tokens=1024,
+        top_p=1,
+        stop=None,
+        stream=False,
     )
     
     return chat_res.choices[0].message.content.strip()
